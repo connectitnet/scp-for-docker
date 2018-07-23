41fa824c6354d878daad1bdb368edcefd7b17cee
Jonathan Senecal <jsenecal@connectitnet.com>
2018-07-23 15:22:57 -0400

    Add support for password authentication

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,20 @@ do
     # Chown home folder (if mounted as a volume for the first time)
     chown -R $username:$username /home/$username
 
+    declare SCP_$username_PWD
+    declare SCP_$username_PWD_FILE
+    user_passwd_var=SCP_$username_PWD
+    user_passwd_file_var=SCP_$username_PWD_FILE
+    
+    if [[ -z "${!user_passwd_file_var}" ]]; then
+        password=$(echo ${!user_passwd_var} | base64 -d) 
+        echo "$username:$password" | chpasswd
+    else
+        password=$(cat ${!user_passwd_file_var})
+        echo "$username:$password" | chpasswd
+    fi
+    
+
     COUNTER=$COUNTER+1
 done
 
